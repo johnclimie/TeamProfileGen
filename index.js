75558567191ecd31ddddf87ac57fb9c932d7491a
John Climie <johnclimie00@gmail.com>
2022-01-13 22:23:55 -0500

    Added HTML variable

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ var start = () => {
                 type: 'input'
             },
             {
-                name: 'managerOfficenumber',
+                name: 'managerOfficeNumber',
                 message: `What is this manager's office number?`,
                 type: 'input'
             },
@@ -44,10 +44,39 @@ var start = () => {
             }
         ])
         .then(function(response) {
-            var newHTMl;
-            const newManager = new Manager(response.managerName, response.managerId, response.managerEmail, response.managerOfficeNumber);
-
-            console.log(response.addOrQuit);
+            var newHTML;
+            let newManager = new Manager(response.managerName, response.managerId, response.managerEmail, response.managerOfficeNumber);
+            newHTML +=
+             `
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+                <title>Your Team</title>
+            </head>
+            <body class='bg-dark'>
+                <div class="jumbotron bg-success">
+                    <h1 class='text-light text-center'>Your Team</h1>
+                </div>
+            
+                <div class='container'>
+                    <div class='row'>
+                        <div class='card p-2 col-3 m-2 bg-light'>
+                            <div class='p-3 border-bottom border-dark'>
+                            <h1 class='card-title'>${newManager.getName()}</h1>
+                            <h2 class='card-subtitle'>${newManager.getRole()}</h2>
+                            </div>
+                            <div class='card-text p-3'>
+                                <p class='card-text'>ID: ${JSON.stringify(newManager.getId())}</p>
+                                <p class='card-text'>Email:<a href="#">${newManager.getEmail()}</a></p>
+                                <p class='card-text'>Office Number: ${JSON.stringify(newManager.getofficeNumber())}</p>
+                            </div>
+                        </div>
+            `;
+            console.log(newHTML);
             
         })
 }
